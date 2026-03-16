3d4ec926e61d9ce7c526179b866e820204423314
Allen Sheng <allen.sheng.2011@gmail.com>
2026-03-16 15:29:12 -0400

    Added self introduction to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Allen Sheng" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print ("I am 14 years old and I am in 9th grade.")
 
 if __name__ == "__main__":
     main()
